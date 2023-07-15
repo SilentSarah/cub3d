8a15eef53a2b197871e6a223adf6639c45992217
onepunchscript <hassimi@c2r4p5.1337.ma>
2023-07-15 15:02:30 +0100

    i have big problem in calculation and i have no idea why

--- cube copy/cub.c
+++ cube copy/cub.c
@@ -6,7 +6,7 @@
 /*   By: hassimi <hassimi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 13:17:11 by hassimi           #+#    #+#             */
-/*   Updated: 2023/07/14 09:24:27 by hassimi          ###   ########.fr       */
+/*   Updated: 2023/07/15 15:00:56 by hassimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,22 @@
 // #define PI 3.14159265
 	mlx_t* mlx;
 
-float PI = 3.14159;
+float PI = M_PI , DR = M_PI / 180;
 static mlx_image_t* image1;
 static mlx_image_t* image2;
 static mlx_image_t* image;
 double rotation_angle = 0.0;
 char **map;
-float cent_y = 240, cent_x = 240;
-float px,py,pa = M_PI_4;
+// double cent_y = 190.005414, cent_x = 199.840863;
+double cent_y = 200, cent_x = 200;
+double px,py,pa = M_PI_4;
 
 
 // -----------------------------------------------------------------------------
 
 int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
 {
-    return (r << 24 | g << 16 | b << 8 | a);
+	return (r << 24 | g << 16 | b << 8 | a);
 }
 
 void rect1(int x, int y)
@@ -171,7 +172,7 @@ void drawline(int x0, int y0, int x1, int y1)
 		mlx_put_pixel(image1,  x0, y0, ft_pixel(255, 0, 0, 255));
         if (x0 == x1 && y0 == y1)
             break;
-        e2 = err;
+        e2 = err * 2;
         if (e2 > -dy)
         {
             err -= dy;
@@ -222,146 +223,189 @@ void my_clear()
 	}
 
 }
-
-void hor_line(int *a, int* b)
+int hardcode(int rvy,int rvx)
 {
-	int rx = 0, ry = 0;
-	int r = 0, mp=0,mx = 0, my = 0,dof;
-	int xo=0,yo =0 ;
-	float ra;
-	float atan= - 1 / tan(ra);
-	ra = pa;
-		dof = 0;
-		if ( ra > PI)
-		{
-			ry = (((int)cent_y / 40)  *40)- 0.0001;
-			printf("ry %d\n",ry);
-			rx = cent_x - (((int)cent_y - ry) / tan(ra));
-			printf("rx %d\n",rx);
-			yo = -40;
-			xo = yo / tan(ra);
-		}
-		if (ra < PI)
-		{
-			ry = (((int)cent_y / 40)  *40) + 40;
-			printf("1ry %d\n",ry);
-			rx = cent_x + (((int)cent_y - ry) / -tan(ra));
-			printf("1rx %d\n",rx);
-			yo = 40;
-			xo = yo / tan(ra);
-		}
-		if ( ra == PI || ra == 0)
-		{
-			rx = cent_x;
-			ry = cent_y;
-			dof = 8;
-		}
-		while(dof < 4)
-		{
-			mx = rx;
-			my = ry;
-			printf("x %d  y %d\n",mx, my );
-			printf("xo %d  yo %d\n",xo, yo );
-			if ((mx > 600) || (my > 400))
-				break;
-			if ((mx < 0) || (my < 0))
-				break;
-			if (map[ry / 40][rx / 40] == '1')
-					break;
-			else
-			{
-				rx +=xo;
-				ry+=yo;
-				dof += 1;
-			}
-		}
-			printf("rx %d  ry %d\n",rx, ry );
-			printf("%c\n",map[ry / 40][rx / 40]);
-			printf("%c\n",map[6][3]);
-			*a = ry;
-			*b = rx;
+	if (pa <= 2 *PI && pa >= (PI + (PI /2))){
+	if ((rvy > 0 && rvy < 560) && (rvx > 0 && rvy < 360))
+	{
+		if (map[rvy / 40 + 1][rvx / 40] == '1' && map[rvy / 40][rvx / 40 - 1] == '1')
+			return (1);
+	}}
+	else if (pa <= (PI + (PI /2)) && pa > PI){
+	if ((rvy > 0 && rvy < 560) && (rvx > 0 && rvy < 360))
+	{
+		if (map[rvy / 40][rvx / 40 + 1] == '1' && map[rvy / 40 + 1][rvx / 40] == '1')
+			return (1);
+	}
+	}
+	else if (pa <= PI && pa >= PI / 2){
+	if ((rvy > 0 && rvy < 560) && (rvx > 0 && rvy < 360))
+	{
+			if (map[rvy / 40][rvx / 40 +1] == '1' && map[rvy / 40 -1][rvx / 40] == '1')
+			return (1);
+
+	}}
+	else if(pa <= PI/2 && pa >= 0){
+	if ((rvy > 0 && rvy < 560) && (rvx > 0 && rvy < 360))
+	{
+		if (map[rvy / 40][rvx / 40 -1] == '1' && map[rvy / 40-1][rvx / 40] == '1')
+			return (1);
+	}}
+	return (0);
 }
 
-void ver_line(int *a, int* b)
+int hardcode1(int rvy,int rvx)
 {
-int r = 0, mp=0,mx = 0, my = 0,dof;
-	int xo=0,yo =0, rvx=0, rvy=0 ;
-	float ra;
-	ra = pa;
-		dof = 0;
-		if ((ra >  PI / 2) && ra < (3*PI/ 2))
+	if (pa <= 2 *PI && pa >= (PI + (PI /2))){
+	if ((rvy > 0 && rvy < 560) && (rvx > 0 && rvy < 360))
+	{
+		if (map[(rvy + 20)/ 40][rvx / 40] == '1' && map[rvy / 40][(rvx - 20)/ 40] == '1')
+			return (1);
+	}}
+	else if (pa <= (PI + (PI /2)) && pa > PI){
+	if ((rvy > 0 && rvy < 560) && (rvx > 0 && rvy < 360))
+	{
+		if (map[rvy / 40][(rvx + 20) / 40] == '1' && map[(rvy + 20) / 40][rvx / 40] == '1')
+			return (1);
+	}
+	}
+	else if (pa <= PI && pa >= PI / 2){
+	if ((rvy > 0 && rvy < 560) && (rvx > 0 && rvy < 360))
+	{
+			if (map[rvy / 40][(rvx + 20) / 40] == '1' && map[(rvy - 20) / 40][rvx / 40] == '1')
+			return (1);
+
+	}}
+	else if(pa <= PI/2 && pa >= 0){
+	if ((rvy > 0 && rvy < 560) && (rvx > 0 && rvy < 360))
+	{
+		if (map[rvy / 40][(rvx - 20) / 40] == '1' && map[(rvy - 20) / 40][rvx / 40] == '1')
+			return (1);
+	}}
+	return (0);
+}
+
+void hor_line(double *a, double* b, double c)
+{
+	double rx = 0, ry = 0;
+	double  mp=0,mx = 0, my = 0;
+	// int dof;
+	double xo=0,yo =0 ;
+	float ra = c;
+	if ( ra > PI)
+	{
+		ry = (((int)(cent_y / 40))  *40)-1;
+		rx = cent_x - ((cent_y - ry) / -(tan(ra)+0.00001));
+		yo = -40;
+		xo = yo / (tan(ra) + 0.00001);
+	}
+	if (ra < PI)
+	{
+		ry = (((int)(cent_y / 40))  *40) + 40;
+		rx = cent_x + ((cent_y - ry) / -(tan(ra)+0.00001));
+		yo = 40;
+		xo = yo / (tan(ra)+0.00001);
+	}
+	while(1)
+	{
+		printf("rx %f  ry %f\n",rx, ry );
+		if (((int)rx > 560) || ((int)ry > 360))
+			break;
+		if (((int)rx < 0) || ((int)ry < 0))
+			break;
+		if (map[(int)(ry / 40)][(int)((rx) / 40)] == '1')
+				break;
+		else
 		{
-			rvx = (((int)cent_x / 40) * 40)- 0.0001;
-			printf("rvy %d\n",rvy);
-			rvy = cent_y - (((int)cent_x - rvx) * tan(ra));
-			printf("rvx %d\n",rvx);
+			rx +=xo;
+			ry+=yo;
+		}
+	}
+		*a = ry;
+		*b = rx;
+}
+
+void ver_line(double *a, double* b, double c)
+{
+int mp=0,mx = 0, my = 0,dof;
+	double xo=0,yo =0, rvx=0, rvy=0 ;
+	double ra = c;
+
+		if ((ra >  PI / 2) || ra < (3*PI/ 2))
+		{
+			rvx = (((int)(cent_x / 40)) * 40) - 0.0001;
+			rvy = cent_y - ((cent_x - rvx) * -tan(ra));
 			xo = -40;
 			yo = xo * tan(ra);
 		}
 		if ((ra <  PI / 2) || ra > (3*PI/ 2))
 		{
-			rvx = (((int)cent_x / 40) * 40) + 40;
-			rvy = cent_y + (((int)cent_x - rvx) * -tan(ra));
+			rvx = (((int)(cent_x / 40)) * 40) + 40;
+			rvy = cent_y + ((cent_x - rvx) * - tan(ra));
 			xo = 40;
 			yo = xo * tan(ra);
 		}
-		if ( ra == PI || ra == 0)
+		// if(ra == M_PI_2 || ra == (M_PI + M_PI_2))
+		// {
+		// 	rvy = cent_y;
+		// 	rvx = cent_x;
+		// }
+		while(1)
 		{
-			rvx = cent_x;
-			rvy = cent_y;
-			dof = 8;
-		}
-		while(dof < 4)
-		{
-			mx = rvx;
-			my = rvy;
-			printf("x %d  y %d\n",mx, my );
-			printf("xo %d  yo %d\n",xo, yo );
-			if ((mx > 600) || (my > 400))
+			printf("rvx %d  rvy %d\n",mx, my );
+			if (hardcode(rvy, rvx) == 1)
 				break;
-			if ((mx < 0) || (my < 0))
+			if ((rvx > 560) || (rvy > 360))
 				break;
-			if (map[rvy / 40][rvx / 40] == '1')
+			if ((rvx < 0) || (rvy < 0))
+				break;
+			if (map[(int)(rvy / 40)][(int)(rvx / 40)] == '1')
 					break;
 			else
 			{
 				rvx +=xo;
 				rvy+=yo;
-				dof += 1;
 			}
 		}
-			printf("rvx %d  rvy %d\n",rvx, rvy );
-			printf("rvxo %d  rvyo %d\n",xo, yo );
-			// printf("%c\n",map[rvy / 40][rvx / 40]);
-			// printf("%c\n",map[6][3]);
 		*a = rvy;
 		*b = rvx;
 }
 
 void drawray()
 {
-	int rx=0,ry=0,rvx=0,rvy =0 ;
-	hor_line(&ry, &rx);
-	ver_line(&rvy, &rvx);
-	int x1 = abs(rx - (int)cent_x);
-	int x2 = abs(ry - (int)cent_y);
-	int dx1;
-	int dx2;
-	if (x1 > x2)
-		dx1 = x1;
-	else
-	 dx1 = x2;
-	x1 = abs(rvx - (int)cent_x);
-	x2 = abs(rvy - (int)cent_y);
-	if (x1 > x2)
-		dx2 = x1;
-	else
-	 dx2 = x2;
-	if (dx2 < dx1 && (rvx > 0 && rvy > 0))
-	// if ((rvx > 0 && rvy > 0))
+	printf("---------\n");
+	double rx=10000,ry=1000,rvx=10000,rvy =10000, i = 0 ;
+	float ra;
+	ra = pa - DR *30;
+	if(ra < 0)
+		ra += 2 * PI;
+	if (ra > 2 * PI)
+		ra -= 2 * PI;
+	while ( i < 60)
+	{
+	hor_line(&ry, &rx, ra);
+	ver_line(&rvy, &rvx, ra);
+	float x1 = 0;
+	x1 = sqrt(pow((rx - cent_x), 2) + pow((ry - cent_y), 2));
+	float x2 = 0;
+	x2 = sqrt(pow((rvx - cent_x), 2) + pow((rvy - cent_y), 2));
+
+	printf("rx %f  ry %f  x1 %f\n",rx, ry, x1 );
+	printf("rvx %f  rvy %f  x1 %f\n",rvx, rvy, x2 );
+	printf("center x %f  center y %f\n",cent_x, cent_y);
+	printf("angle %f\n",pa);
+
+	if (x2 < x1)
 		drawline1(cent_x, cent_y, rvx, rvy);
-	else if (dx2 > dx1 && (rx > 0 && ry > 0))
+	else if (x2 > x1)
 	 drawline1(cent_x, cent_y, rx, ry);
+	 i++;
+	 ra += DR;
+		if(ra < 0)
+			ra += 2 * PI;
+		if (ra > 2 * PI)
+			ra -= 2 * PI;
+	}
 }
 void ft_randomize(void *parm)
 {
@@ -374,44 +418,71 @@ void ft_randomize(void *parm)
 
 void ft_hook(mlx_key_data_t keydata, void* param)
 {
+	double xtemp = 0, ytemp = 0;
 	if (keydata.key == MLX_KEY_ESCAPE)
 		exit(0);
 	if (keydata.key == MLX_KEY_UP)
 	{
-		cent_x +=cos(pa)*5;
-		cent_y +=sin(pa)*5;
+		xtemp =cent_x + cos(pa)*5;
+		ytemp = cent_y + sin(pa)*5;
+		if (map[(int)(ytemp /40)][(int)xtemp / 40] != '1' )
+		{
+			if((int)xtemp / 40!=(int)cent_x / 40 && (int)ytemp / 40!=(int)cent_y / 40 && map[(int)ytemp /40][(int)cent_x / 40] == '1'&& map[(int)cent_y /40][(int)xtemp / 40] == '1' )
+				return;
+			cent_x =xtemp;
+			cent_y =ytemp;
+		}
 
 	}
 	if (keydata.key == MLX_KEY_DOWN)
 	{
-		if (map[(int)cent_y /40][(int)cent_x / 40] != '1')
+		xtemp =cent_x - cos(pa)*5;
+		ytemp = cent_y - sin(pa)*5;
+		if (map[(int)ytemp /40][(int)xtemp / 40] != '1')
 		{
-			cent_x -=cos(pa)*5;
-			cent_y -=sin(pa)*5;
+			if((int)xtemp / 40!=(int)cent_x / 40 && (int)ytemp / 40!=(int)cent_y / 40 && map[(int)ytemp /40][(int)cent_x / 40] == '1'&& map[(int)cent_y /40][(int)xtemp / 40] == '1' )
+					return;
+				cent_x =xtemp;
+				cent_y =ytemp;
 		}
 	}
 	if (keydata.key == MLX_KEY_RIGHT)
 	{
-		cent_y +=cos(-pa)*5;
-		cent_x +=sin(-pa)*5;
+		ytemp =cent_y + cos(-pa)*5;
+		xtemp =cent_x + sin(-pa)*5;
+		if (map[(int)ytemp /40][(int)xtemp / 40] != '1')
+		{
+			if((int)xtemp / 40!=(int)cent_x / 40 && (int)ytemp / 40!=(int)cent_y / 40 && map[(int)ytemp /40][(int)cent_x / 40] == '1'&& map[(int)cent_y /40][(int)xtemp / 40] == '1' )
+				return;
+			cent_x =xtemp;
+			cent_y =ytemp;
+		}
 	}
 	if (keydata.key == MLX_KEY_LEFT)
 	{
-		cent_y -=cos(-pa)*5;
-		cent_x -=sin(-pa)*5;
+		ytemp = cent_y - cos(-pa)*5;
+		xtemp = cent_x - sin(-pa)*5;
+		if (map[(int)ytemp /40][(int)xtemp / 40] != '1')
+		{
+			if((int)xtemp / 40!=(int)cent_x / 40 && (int)ytemp / 40!=(int)cent_y / 40 && map[(int)ytemp /40][(int)cent_x / 40] == '1'&& map[(int)cent_y /40][(int)xtemp / 40] == '1' )
+				return;
+			cent_x = xtemp;
+			cent_y = ytemp;
+
+		}
 	}
 	if (keydata.key == MLX_KEY_A)
 	{
-		pa -= 0.01;
-		if (pa < 0)
+		pa -= 0.07;
+		if (pa <= 0)
 			pa+=2*PI;
 		px = cos(pa) * 10;
 		py = sin(pa) * 10 ;
 	}
 	if (keydata.key == MLX_KEY_D)
 	{
-		pa += 0.01;
-		if (pa > 2 * PI)
+		pa += 0.07;
+		if (pa >= 2 * PI)
 			pa-=2*PI;
 		px = cos(pa) * 10;
 		py = sin(pa) * 10;
@@ -425,17 +496,17 @@ int32_t main(int32_t argc, const char* argv[])
 
 	map = malloc(sizeof(char) * 10 + 1);
 	map[0] = "111111111111111";
-	map[1] = "100000001000001";
-	map[2] = "100000001000001";
-	map[3] = "100000001000001";
-	map[4] = "100000000100001";
+	map[1] = "100000001010001";
+	map[2] = "100000001010001";
+	map[3] = "100000001010001";
+	map[4] = "100000010100001";
 	map[5] = "100100000000001";
-	map[6] = "100100000000001";
-	map[7] = "100000001000001";
+	map[6] = "100100010000011";
+	map[7] = "100000001000101";
 	map[8] = "100000000000001";
 	map[9] = "111111111111111";
 	map[10] = 0;
-	pa = -0.8;
+	pa = DR * (90 + 180);
 	px = cos(pa) * 40;
 	py = sin(pa) * 40;
 	// Gotta error check this stuff
